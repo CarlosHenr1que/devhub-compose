1e987e65f194e193dde45173e9604ca60e875fc5
carlos.compasso <carlos.compasso@fleetcor.com.br>
2022-06-23 12:23:08 -0300

    feat: add github profile information

--- app/src/main/java/com/example/alurachallenge/MainActivity.kt
+++ app/src/main/java/com/example/alurachallenge/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.alurachallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.alurachallenge.ui.theme.AluraChallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +24,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AluraChallengeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Avatar()
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "Carlos Henrique Matos Borges",
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = "CarlosHenr1que")
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = "Mobile Software Developer")
+                        Text(text = "at Compass")
+                    }
                 }
             }
         }
@@ -30,14 +52,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Avatar() {
+    Image(
+        painter = painterResource(id = R.drawable.profile),
+        contentDescription = "Avatar",
+        modifier = Modifier.clip(RoundedCornerShape(10.dp))
+    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AluraChallengeTheme {
-        Greeting("Android")
-    }
-}
